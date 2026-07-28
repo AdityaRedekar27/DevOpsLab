bc51e186d0e6f3c332889a1a25f037893ed43c19
Dev Patel <dev@gmail.com>
2026-07-28 13:47:17 +0530

    Yash made the changes

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        while(true){
+       System.out.println("Rohan");
+        }
     }
 }
